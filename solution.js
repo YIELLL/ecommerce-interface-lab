f6667965670ab773bbae4ec9bbfe44fa7ee81907
YIELLL <kenielddeasis@gmail.com>
2026-03-16 22:47:54 +0800

    Add The Private Inventory Feature

--- solution.js
+++ solution.js
@@ -1,6 +1,15 @@
-function getTopScorers(playerList) {
-    return playerList
-        .filter(player => player.score > 8)
-        .map(player => player.name)
-        .join(", ");
+class Item {
+    #discount = 0.1; 
+
+    constructor(name, price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    get finalPrice() {
+        return this.price - (this.price * this.#discount);
+    }
 }
+
+const laptop = new Item("laptop", 1000);
+console.log(laptop.finalPrice);
