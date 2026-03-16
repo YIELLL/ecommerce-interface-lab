cecccb2d416671af49443aca1bc72f8be520e9aa
YIELLL <kenielddeasis@gmail.com>
2026-03-16 22:36:22 +0800

    Add Leaderboard Filter Feature

---
+++ solution.js
@@ -0,0 +1,12 @@
+const players = [
+    { name: "Jovencio", score: 10 },
+    { name: "Jeanne", score: 5 },
+    { name: "Ian", score: 9 },
+    { name: "Drew", score: 12 },
+    { name: "Shem", score: 7 },
+    { name: "Mark", score: 8 },
+    { name: "Jose", score: 15 },
+    { name: "Rhea", score: 4 },
+    { name: "Papong", score: 11 },
+    { name: "Fergs", score: 6 }
+];
